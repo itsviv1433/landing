e6f4a2f44b0d7e2a1cf98459b4e0378fc5d2ea06
Vivek Mahajan <91725875+itsviv1433@users.noreply.github.com>
2023-08-07 23:33:24 +0530

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $database = 'landing';
+    
+    $conn = mysqli_connect($host,$user,$pass,$database);
+    // if($conn){
+    //     echo "successful";
+    // }else{
+    //     echo "failed";
+    // }
+
+
+?>
